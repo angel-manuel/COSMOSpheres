0ff913433896c404c4e15ca275381d8feda75e58
angel-manuel <spirit5556@hotmail.com>
2013-09-13 17:03:35 +0200

    aaa.cpp añadido solo para defines globales

---
+++ aaa.cpp
@@ -0,0 +1,41 @@
+#define MAX_TORQUE 0.13f
+#define MAX_FORCE 0.26f
+
+#define MAX_NET_DIST 0.45f
+
+#define SPHERE_RADIUS 0.11f
+#define DEBRIS_RADIUS 0.03f
+
+#define MIN_LASSO_DIST 0.14f
+#define MAX_LASSO_DIST 0.20f
+
+#define MAX_ITEM_START_DIST 0.05f
+#define MAX_ITEM_START_VEL 0.01f
+#define MAX_ITEM_START_RATE 0.0401f
+#define MAX_ITEM_RATE 1.3962f
+
+//Indices de los valores en ZRState y similares
+#define POS 0
+#define POS_X 0
+#define POS_Y 1
+#define POS_Z 2
+#define VEL 3
+#define VEL_X 3
+#define VEL_Y 4
+#define VEL_Z 5
+#define ATT 6
+#define ATT_X 6
+#define ATT_Y 7
+#define ATT_Z 8
+#define RATE 9
+//Los siguiente ya están definidos
+//#define RATE_X 9
+//#define RATE_Y 10
+//#define RATE_Z 11
+
+#define NUMBER_OF_DEBRIS 10
+
+#define NUMBER_OF_ITEMS 2
+
+#define OUR 0
+#define THEIR 1
--- main.cpp
+++ main.cpp
@@ -1,67 +1,25 @@
 int seconds;	//Segundos de juego pasados
 int phase;		//Fase de juego actual
 
-#define MAX_TORQUE 0.13f
-#define MAX_FORCE 0.26f
-
-#define MAX_NET_DIST 0.45f
-
-#define SPHERE_RADIUS 0.11f
-#define DEBRIS_RADIUS 0.03f
-
-#define MIN_LASSO_DIST 0.14f
-#define MAX_LASSO_DIST 0.20f
-
-#define MAX_ITEM_START_DIST 0.05f
-#define MAX_ITEM_START_VEL 0.01f
-#define MAX_ITEM_START_RATE 0.0401f
-#define MAX_ITEM_RATE 1.3962f
-
 //update()
 bool blue_sphere;			//Indica si somos la SPHERE azul
-
-//Indices de los valores en ZRState y similares
-#define POS 0
-#define POS_X 0
-#define POS_Y 1
-#define POS_Z 2
-#define VEL 3
-#define VEL_X 3
-#define VEL_Y 4
-#define VEL_Z 5
-#define ATT 6
-#define ATT_X 6
-#define ATT_Y 7
-#define ATT_Z 8
-#define RATE 9
-//Los siguiente ya están definidos
-//#define RATE_X 9
-//#define RATE_Y 10
-//#define RATE_Z 11
 
 ZRState our_state;		//El estado de nuestra SPHERE
 ZRState their_state;	//El estado de su SPHERE
 
 bool is_net_broken;		//Indica si la red está rota
 
-#define NUMBER_OF_DEBRIS 10
-#define NUMBER_OF_DEBRIS_COORDINATES 3*NUMBER_OF_DEBRIS
 bool is_debris_collected[10];	//Indica si alguien ha recogido cierta escombrera
 bool is_debris_ours[10];			//Indica si nosotros hemos recogido cierta escombrera
 bool is_debris_theirs[10];		//Indica si ellos han recogido cierta escombrera
 
-float debris_position[NUMBER_OF_DEBRIS_COORDINATES];	//Contiene las posiciones de las escombreras
+float debris_position[NUMBER_OF_DEBRIS*3];	//Contiene las posiciones de las escombreras
 
-#define NUMBER_OF_ITEMS 2
-#define NUMBER_OF_ITEMS_COORDINATES 3*NUMBER_OF_ITEMS
 bool is_item_collected[NUMBER_OF_ITEMS];	//Indica si alguien ha recogido cierto item
 bool is_item_ours[NUMBER_OF_ITEMS];				//Indica si nosotros hemos recogido cierto item
 bool is_item_theirs[NUMBER_OF_ITEMS];			//Indica si ellos han recogido cierto item
 
-float item_position[NUMBER_OF_ITEMS_COORDINATES];	//Contiene las posiciones de los items
-
-#define OUR 0
-#define THEIR 1
+float item_position[NUMBER_OF_ITEMS*3;	//Contiene las posiciones de los items
 
 float our_comet_state[6];			//Estado de nuestro cometa ({pos}{vel})
 float their_comet_state[6];		//Estado de su cometa ({pos}{vel})
