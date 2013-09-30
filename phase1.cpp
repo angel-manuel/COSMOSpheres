f3e0c8e28ba30c2ae7ee8d69a9de1a707b129609
angel-manuel <spirit5556@hotmail.com>
2013-09-30 19:53:07 +0200

    DIMENSION and phase2_predict() removed

--- phase1.cpp
+++ phase1.cpp
@@ -68,17 +68,17 @@ bool phase1_take(int target_item) {
 //phase1_moveto
 //dst -> Destino
 //return -> true si ya se ha llegado, false en todos los demas casos
-bool phase1_moveto(float *dst) {
-	float delta[DIMENSION];
+bool phase1_moveto(float dst[3]) {
+	float delta[3];
 	float d;
 	float v;
 	float r;
 	float zero[3] = {0.0f};
 
-	mathVecSubtract(delta, dst, &our_state[POS], DIMENSION);
-	d = mathVecMagnitude(delta, DIMENSION);
-	v = mathVecMagnitude(&our_state[VEL], DIMENSION);
-	r = mathVecMagnitude(&our_state[RATE], DIMENSION);
+	mathVecSubtract(delta, dst, &our_state[POS], 3);
+	d = mathVecMagnitude(delta, 3);
+	v = mathVecMagnitude(&our_state[VEL], 3);
+	r = mathVecMagnitude(&our_state[RATE], 3);
 
 	if(d < MAX_ITEM_START_DIST) {
 		if(v < MAX_ITEM_START_VEL && r < MAX_ITEM_START_RATE) {
