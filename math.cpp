7f8fbcab8a985be574430be50d27e414aed0bc56
Adrian1987 <adurpo@gmail.com>
2013-10-01 12:32:38 +0200

    Update math.cpp
    Ángel te he puesto algunos comentarios para el resto de compañeros, ya que muchos no están familiarizados con las herramientas básicas del álgebra.

--- math.cpp
+++ math.cpp
@@ -1,32 +1,36 @@
+//Producto de un vector a, por el escalar b. Resultado vector c.
 void mathVecScalarMult(float *c, float *a, float b, int n) {
 	int i;
 	for(i = 0; i < n; ++i) {
 		c[i] = a[i] * b;
 	}
 }
-
+//Para dismiuir el número de funciones, creo que esta es redundante.
 void mathVecScalarDiv(float *c, float *a, float b, int n) {
 	int i;
 	for(i = 0; i < n; ++i) {
 		c[i] = a[i] / b;
 	}
 }
-
+//Para normalizar el vector v. Propongo un cambio tonto para solo usar la función mathVecScalarMult. Recuerdardtambién
+//la función mathVecNormalize(float *v, n) que ya esta definida.
 void mathVecGetNormal(float *norm, float *v, int n) {
 	mathVecScalarDiv(norm, v, mathVecMagnitude(v, n), n);
+	//mod= 1/mathVecMagnitude(v, n);
+	//mathVecScalarMult(norm, v, mod, n);  
 }
-
+//Copiar vecotres. Entrada src, salida dst.
 void mathVecCopy(float *dst, float *src, int n) {
 	int i;
 	for(i =0; i < n; ++i) {
 		dst[i] = src[i];
 	}
 }
-
+//Valor absoluto de un número.
 float absf(float val) {
 	return (val > 0) ? val : -val;
 }
-
+//Función signo.
 float signf(float val) {
 	if(val > 0.0f) {
 		return 1.0f;
