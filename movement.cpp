180eae89e2936276fe62af438280331344f64c68
angel-manuel <spirit5556@hotmail.com>
2013-10-11 20:38:43 +0200

    Trabajando

--- movement.cpp
+++ movement.cpp
@@ -4,35 +4,24 @@
 //return -> true si ya se ha llegado, false en todos los demas casos
 bool movement_moveto(float dst[3], float tvel[3]) {
 	float delta[3];
-	float delta_vel[3];
 	float d;
-	float v;
-	float r;
-	float zero[3] = {0.0f};
+	float head[3];
 
-	mathVecSubtract(delta, dst, &our_state[POS], 3);
-	d = mathVecMagnitude(delta, 3);
-	mathVecSubtract(delta_vel, tvel, &our_state[VEL], 3);
-	v = mathVecMagnitude(delta_vel, 3);
-	r = mathVecMagnitude(&our_state[RATE], 3);
+	float head_vel[3];
+	float side_vel[3];
+	float head_tvel[3];
+	float side_tvel[3];
 
-	if(d < MAX_ITEM_START_DIST) {
-		if(v < MAX_ITEM_START_VEL && r < MAX_ITEM_START_RATE) {
-			return true;
-		} else {
-			api.setPositionTarget(dst);
-			api.setVelocityTarget(tvel); //TODO: Mejorar para tvel != 0
-			api.setAttRateTarget(zero);
-			return false;
-		}
-	} else {
-		//TODO: Sustituir setPositionTarget
-		//iss5: Maniobrar entre las nubes
-		api.setPositionTarget(dst);
-		api.setVelocityTarget(tvel);
-		api.setAttRateTarget(zero);
-		return false;
-	}
+	mathVecSubtract(delta, dst, &our_state[POS], 3);		//delta = dst - pos
+	d = mathVecMagnitude(delta, 3);							//d = |delta|
+
+	mathVecGetNormal(head, delta, 3);						//head = delta / |delta|
+
+	mathVecMult(head_vel, head, vel, 3);					//head_vel = head * vel
+	mathVecSubtract(side_vel, vel, head_vel, 3);			//side_vel = vel - head_vel
+
+	mathVecMult(head_tvel, head, tvel, 3);					//head_tvel = head * tvel
+	mathVecSubtract(side_tvel, tvel, head_tvel, 3);			//side_tvel = tvel - head_tvel
 }
 
 //movement_timeto
