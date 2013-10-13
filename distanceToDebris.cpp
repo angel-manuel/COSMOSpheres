3ccff46ac26dcdd38e05b5dfa3b7440422dc91fc
angel-manuel <spirit5556@hotmail.com>
2013-10-13 18:43:41 +0200

    Editando con despotismo
    Corrijo la inicialización de vector1 y vector2

--- distanceToDebris.cpp
+++ distanceToDebris.cpp
@@ -8,13 +8,16 @@ float distance[3];
 //vd = Vector Director
 //po = Punto a comprobar(DEBRIS)
 //retorno = vector hasta el Debris(el cuarto valor es la distancia)
-
 void distanceToDebris(float x0[3], float vd[3], float po[3], float retorno[4]){
   
-  vector1 = {-(1/-vd[POS_Y]) - (po[POS_X])};
-  vector2 = {-(1/-vd[POS_Z]) - (po[POS_Y])};
+  float vector1[2] = {-(1/vd[POS_Y]), (po[POS_X])};
+  float vector2[2] = {-(1/vd[POS_Z]), (po[POS_Y])};
+  
+  /*
   distance = sqrt(vector1^2 + vector2^2);
   retorno[4] = mathVecMagnitude(distance, 3);
+  */
+  //Supongo que esto seria necesario luego :)
   
   retorno[POS_X] = distance[POX_X];
   retorno[POS_Y] = distance[POS_Y];
