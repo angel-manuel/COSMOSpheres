415afb351f47145572593ab7b72fc1ebd322d2e7
Wxalex98 <wxalex98@gmail.com>
2013-10-13 18:05:06 +0200

    Update distanceToDebris.cpp

--- distanceToDebris.cpp
+++ distanceToDebris.cpp
@@ -1,1 +1,22 @@
 //Calcular la distancia del vector desde el punto inicial hasta la primera colisión con un debris.
+
+float vector1[2];
+float vector2[2];
+float distance[3];
+
+//x0 = Punto de la Recta(posicion inicial)
+//vd = Vector Director
+//po = Punto a comprobar(DEBRIS)
+//retorno = vector hasta el Debris(el cuarto valor es la distancia)
+
+void distanceToDebris(float x0[3], float vd[3], float po[3], float retorno[4]){
+  
+  vector1 = {-(1/-vd[POS_Y]) - (po[POS_X])};
+  vector2 = {-(1/-vd[POS_Z]) - (po[POS_Y])};
+  distance = sqrt(vector1^2 + vector2^2);
+  retorno[4] = mathVecMagnitude(distance, 3);
+  
+  retorno[POS_X] = distance[POX_X];
+  retorno[POS_Y] = distance[POS_Y];
+  retorno[POS_Z] = distance[POS_Z];
+}
