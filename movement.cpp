9c86c342d52ebb18b50aced61d0903f2d9a1462e
angel-manuel <spirit5556@hotmail.com>
2013-10-14 20:01:05 +0200

    Añadiendo código

--- movement.cpp
+++ movement.cpp
@@ -4,16 +4,53 @@
 //return -> true si ya se ha llegado, false en todos los demas casos
 bool movement_moveto(float dst[3]) {
 	float delta[3];
-	float d;
 	float head[3];
 
 	mathVecSubtract(delta, dst, &our_state[POS], 3);		//delta = dst - pos
-	d = mathVecMagnitude(delta, 3);							//d = |delta|
+	
+	if(mathVecMagnitude(delta, 3) < MAX_ITEM_START_DIST && mathVecMagnitude(&our_state[VEL]) < MAX_ITEM_START_VEL) {
+		return true;
+	}
 
 	mathVecGetNormal(head, delta, 3);						//head = delta / |delta|
 
 	int debris_number;
-	for(debris_number = 0; )
+	float debris_distance[4];
+	float debris_d;
+	int nearest_debris = -1;
+	float nearest_debris_distance = 1000000.0f;
+	float* nearest_debris_vector;
+	for(debris_number = 0; debris_number < NUMBER_OF_DEBRIS; debris_number++) {
+		if(!is_debris_collected[debris_number]) {
+			distanceToDebris(&our_state[POS], head, debris_position[debris_number], debris_distance);
+			debris_d = mathVecMagnitude(debris_distance, 3) - (SPHERE_RADIUS + DEBRIS_RADIUS + 0.01f);
+			if(debris_d < 0) {
+				//Colisión
+				if(debris_distance[3] < nearest_debris_distance && debris_distance[3] > 0.0f) {
+					nearest_debris = debris_number;
+					nearest_debris_vector = debris_distance;
+					nearest_debris_distance = debris_distance[3];
+				}
+			}
+		}
+	}
+
+	if(nearest_debris >= 0 && nearest_debris < NUMBER_OF_DEBRIS) { //Si nearest_debris es un debris válido
+		float max_penetration_point[3];
+		float tmp[3];
+		float next[3];
+
+		mathVecScalarMult(tmp, head, nearest_debris_distance, 3);
+		mathVecAdd(max_penetration_point, &our_state[POS], tmp, 3);
+		mathVecScalarMult(tmp, nearest_debris_vector, -nearest_debris_distance, 3); //Correcion a aplicar
+		mathVecAdd(next, max_penetration_point, tmp, 3);
+
+		api.setPositionTarget(next);
+	} else {
+		api.setPositionTarget(dst);
+	}
+
+	return false;
 }
 
 //movement_timeto
