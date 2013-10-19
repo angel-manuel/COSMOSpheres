83b2c01bc03dbf9c5ae23f50d52a6cb02664d25c
angel-manuel <spirit5556@hotmail.com>
2013-10-19 11:08:12 +0200

    Indeciso

--- movement.cpp
+++ movement.cpp
@@ -5,6 +5,7 @@
 bool movement_moveto(float dst[3]) {
 	float delta[3];
 	float head[3];
+	const float correction = (SPHERE_RADIUS + DEBRIS_RADIUS + 0.1f);
 
 	mathVecSubtract(delta, dst, &our_state[POS], 3);		//delta = dst - pos
 	
@@ -15,38 +16,37 @@ bool movement_moveto(float dst[3]) {
 	mathVecGetNormal(head, delta, 3);						//head = delta / |delta|
 
 	int debris_number;
-	float debris_distance[4];
-	float debris_d;
+	float debris_vector[5];
 	int nearest_debris = -1;
 	float nearest_debris_distance = 1000000.0f;
 	float* nearest_debris_vector;
 	for(debris_number = 0; debris_number < NUMBER_OF_DEBRIS; debris_number++) {
 		if(!is_debris_collected[debris_number]) {
-			distanceToDebris(&our_state[POS], head, debris_position[debris_number], debris_distance);
-			debris_d = mathVecMagnitude(debris_distance, 3) - (SPHERE_RADIUS + DEBRIS_RADIUS + 0.01f);
-			if(debris_d < 0) {
+			distanceToDebris(&our_state[POS], head, debris_position[debris_number], debris_vector);
+			if(debris_vector[4] - correction < 0.0f) {
 				//Colisión
-				if(debris_distance[3] < nearest_debris_distance && debris_distance[3] > 0.0f) {
+				//DEBUG(("%i:[%f, %f, %f] at %f:%f\n", debris_number, debris_position[debris_number][POS_X], debris_position[debris_number][POS_Y], debris_position[debris_number][POS_Z], debris_vector[4], debris_vector[3]));
+				if(debris_vector[3] < nearest_debris_distance && debris_vector[3] > 0.0f) {
 					nearest_debris = debris_number;
-					nearest_debris_vector = debris_distance;
-					nearest_debris_distance = debris_distance[3];
+					nearest_debris_vector = debris_vector;
+					nearest_debris_distance = debris_vector[3];
 				}
 			}
 		}
 	}
 
-	if(nearest_debris >= 0 && nearest_debris < NUMBER_OF_DEBRIS) { //Si nearest_debris es un debris válido
-		float max_penetration_point[3];
+	if(nearest_debris >= 0 && nearest_debris < NUMBER_OF_DEBRIS && nearest_debris_distance < mathVecMagnitude(delta, 3)) { //Si nearest_debris es un debris válido
 		float tmp[3];
 		float next[3];
 
-		mathVecScalarMult(tmp, head, nearest_debris_distance, 3);
-		mathVecAdd(max_penetration_point, &our_state[POS], tmp, 3);
-		mathVecScalarMult(tmp, nearest_debris_vector, -nearest_debris_distance, 3); //Correcion a aplicar
-		mathVecAdd(next, max_penetration_point, tmp, 3);
+		mathVecScalarMult(tmp, nearest_debris_vector, correction, 3);
+		mathVecAdd(next, debris_position[nearest_debris], tmp, 3);
 
+		DEBUG(("debris %i = [%f, %f, %f] at %f\n", nearest_debris, debris_position[nearest_debris][POS_X], debris_position[nearest_debris][POS_Y], debris_position[nearest_debris][POS_Z], nearest_debris_distance));
+		DEBUG(("next = [%f, %f, %f]\n", next[POS_X], next[POS_Y], next[POS_Z]));
 		api.setPositionTarget(next);
 	} else {
+		DEBUG(("Clear!\n"));
 		api.setPositionTarget(dst);
 	}
 
--- phase1.cpp
+++ phase1.cpp
@@ -7,11 +7,11 @@ void phase1_init() {
 }
 
 void phase1_loop() {
-	const int target_item = (blue_sphere) ? 0 : 1;
+	const int target_item = (blue_sphere) ? 1 : 0;
+	const int target_item2 = (blue_sphere) ? 0 : 1;
 
 	if(phase1_take(target_item)) {
-		float dst[3] = {our_comet_state[POS_X] + (blue_sphere) ? -0.18f : 0.18f, 0.7f, our_comet_state[POS_Z]};
-		api.setPositionTarget(dst);
+		phase1_take(target_item2);
 	}
 	
 }
