c1c987549a0a7844d30a0f9f6a048acee4445ea5
angel-manuel <spirit5556@hotmail.com>
2013-10-22 21:50:33 +0200

    distanceToDebris fixed

--- distanceToDebris.cpp
+++ distanceToDebris.cpp
@@ -12,15 +12,19 @@ void distanceToDebris(float x1[3], float x2[3], float p[3], float ret[5]){
   mathVecSubtract(x2_x1, x2, x1, 3);
 
   float d = mathVecMagnitude(x2_x1, 3);
-  float t = mathVecInner(x1_p, x2_x1, 3)/(d*d);
+  float cxs[3];
+  float one[3] = {1.0f, 1.0f, 1.0f};
+  mathVecCross(cxs, x1_p, x2_x1);
+  float t = mathVecInner(cxs, one, 3)/(d*d);
 
-  ret[POS_X] = ((x1[POS_X]-p[POS_X])+(x2[POS_X]-x1[POS_X]))*t;
-  ret[POS_Y] = ((x1[POS_Y]-p[POS_Y])+(x2[POS_Y]-x1[POS_Y]))*t;
-  ret[POS_Z] = ((x1[POS_Z]-p[POS_Z])+(x2[POS_Z]-x1[POS_Z]))*t;
+  ret[POS_X] = (x1[POS_X]-p[POS_X])+(x2[POS_X]-x1[POS_X])*t;
+  ret[POS_Y] = (x1[POS_Y]-p[POS_Y])+(x2[POS_Y]-x1[POS_Y])*t;
+  ret[POS_Z] = (x1[POS_Z]-p[POS_Z])+(x2[POS_Z]-x1[POS_Z])*t;
 
   ret[3] = t;
   ret[4] = mathVecNormalize(ret, 3);
 
+  /*
   #ifdef DEBUG_ACTIVE
   if(seconds == 0) {
   DEBUG(("x1 = "));debug_print_vector(x1, 3);
@@ -29,4 +33,5 @@ void distanceToDebris(float x1[3], float x2[3], float p[3], float ret[5]){
   DEBUG(("ret = "));debug_print_vector(ret, 5);
   }
   #endif
+  */
 }
