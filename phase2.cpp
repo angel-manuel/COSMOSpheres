68ecc35d46d14f964fe68673db6ffdbe04c3a1fe
angel-manuel <spirit5556@hotmail.com>
2013-10-24 20:47:52 +0200

    EXTERMINATE

--- phase2.cpp
+++ phase2.cpp
@@ -1,10 +1,4 @@
-float target_att[3];
-
 void phase2_init() {
-	target_att[0] = (blue_sphere) ? -1.0f : 1.0f;
-	target_att[1] = (blue_sphere) ? -1.0f : 1.0f;
-	target_att[2] = 0.0f;
-	mathVecNormalize(target_att, 3);
 }
 
 void phase2_loop() {
@@ -29,29 +23,39 @@ void phase2_loop() {
 //phase2_prepare
 //Se coloca en posición para disparar al cometa por primera vez
 void phase2_prepare() {
-	float target_pos[3] = {(blue_sphere) ? 0.4f : -0.4f, 0.7f, (blue_sphere) ? 0.4f : -0.4f};
-	//float target_pos[3] = {our_state[POS_X], 0.5f, our_state[POS_Z]};
+	float target_pos[3] = {(blue_sphere) ? 0.3f : -0.3f, 0.2f, (blue_sphere) ? -0.6f : 0.6f};
+	float target_att[3] = {(blue_sphere) ? -0.3f : 0.3f, 0.7f, (blue_sphere) ? 0.7f : -0.7f};
+	mathVecNormalize(target_att, 3);
 
 	api.setPositionTarget(target_pos);
-	//api.setAttitudeTarget(target_att);
+	api.setAttitudeTarget(target_att);
 }
 
 bool phase2_follow() {
 	float raycast[5];
-	distanceToDebris(&our_state[POS], &our_state[ATT], &our_comet_state[POS], raycast);
+	float head[3];
+	mathVecAdd(head, &our_state[POS], &our_state[ATT], 3);
+	distanceToDebris(&our_state[POS], head, &our_comet_state[POS], raycast);
 
 	if(raycast[4] < COMET_RADIUS) {
-		api.setVelocityTarget(&our_comet_state[VEL]);
-		//api.setAttitudeTarget(target_att);
 		return true;
 	}
 
 	#ifdef DEBUG_ACTIVE
 	float angle = atan2f(raycast[4], raycast[3]);
-	DEBUG(("angle = %f\n", angle));
+	DEBUG(("phase2:angle = %f\n", angle));
+	DEBUG(("phase2_dist = %f\n", raycast[4]));
 	#endif
 
-	api.setVelocityTarget(&our_comet_state[VEL]);
-	//api.setAttitudeTarget(target_att);
+	float target_att[3];
+	float fut_state[6];
+	game.predictCometState(1, our_comet_state, fut_state);
+	mathVecSubtract(target_att, &fut_state[POS], &our_state[POS], 3);
+	mathVecNormalize(target_att, 3);
+
+	float zero[3] = {0.0f};
+	api.setVelocityTarget(zero);
+	api.setAttitudeTarget(target_att);
+
 	return false;
 }
