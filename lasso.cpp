b7f61e9a829397ff31a04d0c3ed975c4791f4dea
angel-manuel <spirit5556@hotmail.com>
2013-11-08 16:15:06 +0100

    Adding lasso.cpp

---
+++ lasso.cpp
@@ -0,0 +1,15 @@
+//lasso
+//Contains functions to perform lassoing
+
+bool lasso_picking;
+int lasso_last_debris;
+float lasso_initial_rel_pos[3];
+
+void lasso_init() {
+	lasso_picking = false;
+	lasso_last_debris = -1;
+}
+
+bool lasso_pick(int debris_number) {
+
+}
