9702658ce45d1993ae13cf96845280627cdeb29c
Salok <nikolas7796@hotmail.com>
2013-11-30 19:06:02 +0100

    Added Kamikaze Trolling, started debugging

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 int seconds;	//Passed time since the begining
 
+//For Kamikaze_Trolling
+//Correction factor
+float delta;
+
+//To check wether we were attacked in the last two loops. Initialize to false.
+bool Kamikaze[3];
+
+//To save the position target
+float movingHere[3];
+
+/*------------------------------------------*/
+
 //update()
 bool blue_sphere;		//It is true if we are the blue sphere and false if we are the red one
 
@@ -77,6 +89,15 @@ void loop_update() {
 //Initializes variables in main and in other files
 void init() {
   seconds = 0;
+
+  	//Initializing Kamikaze_Trolling
+	delta = 0.05f;
+	for(int i = 0; i<3; i++)
+	{
+		Kamikaze[i] = false;
+		movingHere[i] = 0;
+	}
+	
 
   #ifdef DEBUG_ACTIVE
   //Only if we are debugging, we initialize the varialbes in the debug file
