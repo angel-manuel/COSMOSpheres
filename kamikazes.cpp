0debfe89ce99324e532e7363726d0bd731a1d4a5
Wxalex98 <wxalex98@gmail.com>
2013-12-01 12:42:44 +0100

    Kamikaze Counter
    Added dodge, push  and main alternPos with a default escape target.

--- kamikazes.cpp
+++ kamikazes.cpp
@@ -9,8 +9,8 @@ void predict_kamikaze(){
 void watch_other_sphere(int timing){	
 	
 	//Variables
-	float myState[3];
-	float otherState[3];
+	float myState[12];
+	float otherState[12];
 	float myPos[3];
 	float otherPos[3];
 	float between[3];
@@ -39,12 +39,62 @@ void watch_other_sphere(int timing){
 		distance2 = actualDistance;
 		case 3:
 		distance3 = actualDistance;
-			break;
+		break;
 	}
-	//If the distances are shortening, we run away (in development, need to establish the alternPos)
-	if(distance3 < distance2 < distance1 < distance0){
-		float alternPos[3] = {0.0f,0.0f,0.0f};
-		api.setPositionTarget(alternPos);	
+	
+	//Are we closer to Item 1 or 2?
+	bool isItItem1;
+	float referencePos[3]= {0.0f, 0.65f, 0.0f};
+	float vecTo1[3];
+	float vecTo2[3];
+	float distanceTo1;
+	float distanceTo2;
+	float firstItemPos[3]= {0.5f, 0.65f,0.0f};
+		float seconItemPos[3]= {-0.5f,0.65f,0.0f};
+	mathVecSubtract(vecTo1, firstItemPos, myPos, 3);
+	mathVecSubtract(vecTo2, seconItemPos, myPos, 3);
+	distanceTo1 = mathVecMagnitude(vecTo1, 3);
+	distanceTo2 = mathVecMagnitude(vecTo2, 3);
+	if(distanceTo1<distanceTo2){
+		isItItem1 = true;
+	}else if(distanceTo2>distanceTo1){
+		isItItem1 = false;
+	}else{
+		api.setPositionTarget(referencePos);//Reference point, in case of bugs
+	}
+	
+	//If the distances are shortening, we run away to one of the items depending on our pos
+	bool dodgeDone;
+	bool isYequal;
+	if((distance3 < distance2 && distance2 < distance1 && distance1 < distance0){
+		#ifdef DEBUG_ACTIVE
+		DEBUG(("KAMIKAZE DETECTED\n"));
+		#endif
+		float alternPos[3];
+		if(isItItem1){
+			alternPos[0] = firstItemPos[0];
+			alternPos[1] = firstItemPos[1];
+			alternPos[2] = firstItemPos[2];
+		}else{
+			alternPos[0] = seconItemPos[0];
+			alternPos[1] = seconItemPos[1];
+			alternPos[2] = seconItemPos[2];
+		}
+		api.setPositionTarget(alternPos);
+		dodgeDone = true;
+	}
+	
+	//Is the Y coordinate equal? (+- 0.1)
+	if((myPos[1] = otherPos[1]) || (myPos[1] = otherPos[1] + 0.1) || (myPos[1] = otherPos[1] - 0.1)){
+		isYequal = true;
+	}
+	
+	//Once we've dodged them, we push them away
+	if(dodgeDone && isYequal){
+		#ifdef DEBUG_ACTIVE
+		DEBUG(("DODGE DONE\n"));
+		#endif
+		api.setPositionTarget(otherPos);
+		DEBUG(("OUT OF MY WAY\n"));
 	}
 }
-
