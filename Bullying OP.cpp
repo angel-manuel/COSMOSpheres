f7404eed33f79677a765c3b0734edf48761b804e
Salok <nikolas7796@hotmail.com>
2013-12-01 12:10:23 +0100

    Cambios en Bullying OP, listo para testear

--- Bullying OP.cpp
+++ Bullying OP.cpp
@@ -15,6 +15,7 @@ void bullyingWithTheNet()
 
 	//To store the diameters of the debris danger sphere.
 	float cube[6];
+	float danger_radius = 0.18;
 
 	//If the net is broken return to main.
 	if(netBroken)
@@ -24,16 +25,16 @@ void bullyingWithTheNet()
 	game.setVelocityTarget(&their_state[3]);
 
 	//If we are about to crash break the net and avoid the debris. Simmetry of the debris field should make them crash if they aren't fast enough.
-	//Sum position and velocity. 
+		//Sum position and velocity. 
 	mathVecAdd(crash, &our_state[0], &our_state[3], 3);
 
-	//If the sum is in the danger radius (0.18) break the net.
+		//If the sum is in the danger radius (0.18) break the net.
 	nearest_debris = nearestDebris();
 	for(int i = 0; i<3; i++)
 	{	
 		//First define the danger zone.
-		cube[2*i]   = debris_position[nearest_debris][i] + 0.18;
-		cube[2*i+1] = debris_position[nearest_debris][i] - 0.18;
+		cube[2*i]   = debris_position[nearest_debris][i] + danger_radius;
+		cube[2*i+1] = debris_position[nearest_debris][i] - danger_radius;
 		//Look if you're in danger
 		if (!(crash[i] < cube[2*i] && crash[i] > cube[2*i+1]))
 			//If you aren't return to main
@@ -47,7 +48,7 @@ void bullyingWithTheNet()
 //When the net breaks we will go to the item as usual, but when they try to get theirs we will push them out of bounds for a while.
 //Then we get our item and just fire the laser. It's almost the same but they will have less fuel. And maybe they won't get their laser.
 //PROFIT.
-void bullyingWithTheItem()
+bool bullyingWithTheItem()
 {
 	float tempVec[3];
 	float otherVec[3];
@@ -67,16 +68,17 @@ void bullyingWithTheItem()
 
 	//If not, return to main (or if we need to back to our item)
 	if(enemy_item == -1 || seconds >= 75)
-		return;
+		return false;
 
 	//If they are, there we go!
 	movement_moveto(item_position[i], false);
+	return true;
 }
 
 //And for the Grand Finale. If we didn't win yet we will try and push the other guy against his comet. 
 //Why would they give us their comet state if it weren't for this?
 
-void bullyingInPhase2()
+bool bullyingInPhase2()
 {
 	//I'm not sure as to when we should use this... I'll be changing this according to what I see on simulations.
 	//First we'll just use it when they are near their comets trying to aim.
@@ -84,13 +86,60 @@ void bullyingInPhase2()
 	//To save their comet state
 	float their_Comet[6];
 	game.getCometState(1, their_Comet);
-	//Check if they are near their comet
+
+	//To save their state and compare it to the danger zone
+	float crash[3];
+	mathVecAdd(crash, &their_state[0], &their_state[3], 3);
+
+	//Define the danger cube
+	float cube[6];
+	float danger_radius = 0.18;
+
+	//Check if they are near their danger cube
+	for(int i = 0; i<3; i++)
+	{
+		//Define the danger cube size
+		cube[2*i] = their_Comet[i] + danger_radius;
+		cube[2*i+1] = their_Comet[i] - danger_radius;
+
+		//Check if they are near it
+		if (!(crash[i] < cube[2*i] && crash[i] > cube[2*i+1]))
+		{
+			//If they aren't return to main
+			return false;
+		}
+	}
 	
-
-
-
+	//They are near their comet, there we go!
+	movement_moveto(&their_state[0], true);
+	return true;
 }
 
+//Extra One! If they are near the OutOfBounds zone we should push them out just a bit don't you think?
+//There we go!
+bool getThemOutOfBounds()
+{
+	//If they are already out there is nothing to do here, we don't want to push them too much
+	if(outOfBounds(&their_state[0]))
+		return false;
+
+	//Save their state
+	float almostOut[3];
+	float margin [3] = {0.1f, 0.1f, 0.1f};
+
+	//Add their position and speed. Add a margin.
+	mathVecAdd(almostOut, &their_state[0], &their_state[3], 3);
+	mathVecAdd(almostOut, almostOut, margin, 3);
+
+	//Check if they are almost out
+	if (outOfBounds(almostOut))
+	{
+		movement_moveto(&their_state[0], true);
+		return true;
+	}
+
+	return false;
+}
 
 //Look for the nearest debris
 int nearestDebris()
@@ -117,4 +166,23 @@ int nearestDebris()
 	return nearest_debris;
 }
 
+//Checks if a vector is out of bounds.
+bool outOfBounds(float objective[3])
+{
+	//Save the game field
+	float gameField[6];
 
+   for(int i = 0; i<3; i++)
+   {
+   	//ZBOUND is not defined. I don't know it's value and the ZR IDE is down
+   	gameField[2*i]  = (i==0 ? 0.64 : ((i==1) ? 0.80 : (ZBOUND)))
+   	gameField[2*i+1]= (i==0 ? -0.64 : ((i==1) ? -0.80 : -(ZBOUND)))
+
+   	//Check if objective (x, y , z) is outside the game field
+   	if((objective[i] > gameField[2*i]) || (objective[i] < gameField[2*i+1]))
+   		return true;
+   }
+
+   return false;
+
+}
--- Kamikaze_Trolling.cpp
+++ Kamikaze_Trolling.cpp
@@ -2,31 +2,25 @@
 
 //To avoid being pushed out by kamikazes we will fall back between the debris to make them crash
 
-//Correction factor
-float delta;
-
-//To check wether we were attacked in the last two loops. Initialize to false.
-bool Kamikaze[3];
-
-//To save the position target
-float movingHere[3]:
-
 //Calculate the difference between the relative position of the spheres and
 //their velocity vector.
 bool Kamikaze_Detecting()
 {
 	//Relative position of both spheres
 	float relative_position [3];
-	mathVecSubtract(relative_position, &our_state[0], &their_state[0]);
-	distance[0] = mathVecNormalize(relative_position, 3);
+	mathVecSubtract(relative_position, &our_state[0], &their_state[0], 3);
+	mathVecNormalize(relative_position, 3);
 
 	//Velocity of enemy sphere
-	float enemy_velocity[3] = &their_state[3];
+	float enemy_velocity[3];
+	enemy_velocity[0] = their_state[3];
+	enemy_velocity[1] = their_state[4];
+	enemy_velocity[2] = their_state[5];
 	mathVecNormalize(enemy_velocity, 3);
 
 	//We compare both
 	float result[3];
-	mathVecSubtract(result, enemy_velocity, relative_position);
+	mathVecSubtract(result, enemy_velocity, relative_position,3);
 
 	//If less than the correction factor, we are being attacked.
 	return (mathVecMagnitude(result, 3) < delta);
@@ -36,7 +30,10 @@ bool Kamikaze_Detecting()
 void Kamikaze_Trolling()
 {
 	//To store the point to which we should move in case of attack.
-	float position_target [3] = &our_state[0];
+	float position_target [3];
+	position_target[0] = our_state[0];
+	position_target[1] = our_state[1];
+	position_target[2] = our_state[2];
 	//Check if we are under attack
 	Kamikaze[0] = Kamikaze_Detecting();
 
@@ -52,7 +49,9 @@ void Kamikaze_Trolling()
 	else if((Kamikaze[0] && Kamikaze[1]) && (!(Kamikaze[2])))
 	{
 		//We save the position target in movingHere. It's near debris. 
-		movingHere = debris_position[nearestDebris()];
+		movingHere[0] = debris_position[nearestDebris()][0];
+		movingHere[1] = debris_position[nearestDebris()][1];
+		movingHere[2] = debris_position[nearestDebris()][2];
 		movingHere[0] += ((our_state[0] > 0) ? 0.18f : -0.18f);
 		movement_moveto(movingHere, false);
 		//We activate troll mode.
@@ -64,7 +63,7 @@ void Kamikaze_Trolling()
 		//While troll mode is active we keep on moving to the point
 		//When we arrive there we get out of troll mode. We will fall back in
 		//if they keep chasing us.
-		Kamikaze[2] = (!(movement_moveto(movingHere)));
+		Kamikaze[2] = (!(movement_moveto(movingHere, false)));
 	}
 
 	Kamikaze[1] = Kamikaze[0];
@@ -76,20 +75,20 @@ int nearestDebris()
 {
 	int nearest_debris = -1;
 	float distance = 999999;
-	float temp_dist;
+	float temp_dist [3];
 
 	//Look for the closest debris to back to.
 	for(int i = 0; i < NUMBER_OF_DEBRIS; i++)
 	{
 		if(!is_debris_collected[i])
 		{
-			mathVecSubtract(temp_dist, &our_state[0], debris_position[i]);
+			mathVecSubtract(temp_dist, &our_state[0], debris_position[i], 3);
 			if(mathVecMagnitude(temp_dist, 3)<distance)
 			{
 				nearest_debris = i;
-				distance = temp_dist;
+				distance = mathVecMagnitude(temp_dist, 3);
 			}
-		]
+		}
 	}
 	return nearest_debris;
 }
