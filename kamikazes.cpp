6dc64b3404b75b6fa8dd64eb488a9e334e31f398
Wxalex98 <wxalex98@gmail.com>
2013-12-01 20:59:54 +0100

    Updated dodger with Salok's detector.
    Copied the detector from Salok's program and structured the code with two bools and a void for dodging.

--- kamikazes.cpp
+++ kamikazes.cpp
@@ -1,100 +1,80 @@
-//Avoid kamikazes
-void predict_kamikaze(){
-	//Loop for checking their state
-	for(int j = 0; j < 3; j++){
-		watch_other_sphere(j);
-	}
+//Let's make those guys bite the dust once again
+
+//Check if a kamikaze is coming, copied from Salok's kamikaze trolling program
+bool predict_Kamikaze()
+{
+	//Relative position of both spheres
+	float relative_position [3];
+	mathVecSubtract(relative_position, &our_state[0], &their_state[0], 3);
+	mathVecNormalize(relative_position, 3);
+
+	//Velocity of enemy sphere
+	float enemy_velocity[3];
+	enemy_velocity[0] = their_state[3];
+	enemy_velocity[1] = their_state[4];
+	enemy_velocity[2] = their_state[5];
+	mathVecNormalize(enemy_velocity, 3);
+
+	//We compare both
+	float result[3];
+	mathVecSubtract(result, enemy_velocity, relative_position,3);
+
+	//If less than the correction factor, we are being attacked.
+	return (mathVecMagnitude(result, 3) < delta);
 }
 
-void watch_other_sphere(int timing){	
-	
-	//Variables
-	float myState[12];
-	float otherState[12];
-	float myPos[3];
-	float otherPos[3];
-	float between[3];
-	float distance0;
-	float distance1;
-	float distance2;
-	float distance3;
-	
-	//Get our position and theirs
-	api.getMyZRState(myState);
-	api.getOtherZRState(otherState);
-	for(int i = 0; i<3; i++){
-		myPos[i] = myState[i];
-		otherPos[i] = otherState[i];
-	}
-	//Calculate vector between them and us
-	mathVecSubtract(between, otherPos, myPos, 3);
-	float actualDistance = mathVecMagnitude(between, 3);
-	//Set distance variables
-	switch(timing){
-		case 0:
-		distance0 = actualDistance;
-		case 1:
-		distance1 = actualDistance;
-		case 2:
-		distance2 = actualDistance;
-		case 3:
-		distance3 = actualDistance;
-		break;
-	}
-	
-	//Are we closer to Item 1 or 2?
-	bool isItItem1;
-	float referencePos[3]= {0.0f, 0.65f, 0.0f};
+//Which is the nearest item?
+bool isItItem1(){
 	float vecTo1[3];
 	float vecTo2[3];
 	float distanceTo1;
 	float distanceTo2;
 	float firstItemPos[3]= {0.5f, 0.65f,0.0f};
-		float seconItemPos[3]= {-0.5f,0.65f,0.0f};
-	mathVecSubtract(vecTo1, firstItemPos, myPos, 3);
-	mathVecSubtract(vecTo2, seconItemPos, myPos, 3);
+	float seconItemPos[3]= {-0.5f,0.65f,0.0f};
+	mathVecSubtract(vecTo1, firstItemPos, &our_state[POS], 3);
+	mathVecSubtract(vecTo2, seconItemPos, &our_state[POS], 3);
 	distanceTo1 = mathVecMagnitude(vecTo1, 3);
 	distanceTo2 = mathVecMagnitude(vecTo2, 3);
-	if(distanceTo1<distanceTo2){
-		isItItem1 = true;
-	}else if(distanceTo2>distanceTo1){
-		isItItem1 = false;
-	}else{
-		api.setPositionTarget(referencePos);//Reference point, in case of bugs
-	}
-	
-	//If the distances are shortening, we run away to one of the items depending on our pos
+	return(distanceTo1<distanceTo2);
+}
+
+//What it says, dodges and pushes
+void dodgeAndPush()
+{
 	bool dodgeDone;
 	bool isYequal;
-	if((distance3 < distance2 && distance2 < distance1 && distance1 < distance0){
+	//Define the alternPos values depending on the nearest Item (when the other sphere goes kamikaze while picking the items)
+	if(predictKamikaze){
 		#ifdef DEBUG_ACTIVE
 		DEBUG(("KAMIKAZE DETECTED\n"));
 		#endif
 		float alternPos[3];
 		if(isItItem1){
-			alternPos[0] = firstItemPos[0];
-			alternPos[1] = firstItemPos[1];
-			alternPos[2] = firstItemPos[2];
+			alternPos[POS_X] = firstItemPos[POS_X];
+			alternPos[POS_Y] = firstItemPos[POS_Y];
+			alternPos[POS_Z] = firstItemPos[POS_Z];
 		}else{
-			alternPos[0] = seconItemPos[0];
-			alternPos[1] = seconItemPos[1];
-			alternPos[2] = seconItemPos[2];
+			alternPos[POS_X] = seconItemPos[POS_X];
+			alternPos[POS_Y] = seconItemPos[POS_Y];
+			alternPos[POS_Z] = seconItemPos[POS_Z];
 		}
 		api.setPositionTarget(alternPos);
 		dodgeDone = true;
+		#ifdef DEBUG_ACTIVE
+		DEBUG(("AT LEAST YOU TRIED\n"));
+		#endif
 	}
 	
 	//Is the Y coordinate equal? (+- 0.1)
-	if((myPos[1] = otherPos[1]) || (myPos[1] = otherPos[1] + 0.1) || (myPos[1] = otherPos[1] - 0.1)){
+	if((our_state[POS_Y] = their_state[POS_Y]) || (our_state[POS_Y] = their_state[POS_Y] + 0.1) || (our_state[POS_Y] = their_state[POS_Y] - 0.1)){
 		isYequal = true;
 	}
 	
 	//Once we've dodged them, we push them away
 	if(dodgeDone && isYequal){
+		api.setPositionTarget(&their_state[POS]);
 		#ifdef DEBUG_ACTIVE
-		DEBUG(("DODGE DONE\n"));
-		#endif
-		api.setPositionTarget(otherPos);
 		DEBUG(("OUT OF MY WAY\n"));
+		#endif
 	}
 }
